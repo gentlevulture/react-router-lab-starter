452afc37aa984265d1591f0fe3434d336dce1dc7
gentlevulture <novemberboughs@gmail.com>
2021-12-08 23:20:25 -0800

    dentistWebsite

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+    return (
+      <div>
+        <h1>About us...</h1>
+        <p>We fill holes in teeth. We fill holes in teeth.</p>
+      </div>
+    );
+  
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Home from './components/Home'
+import Contact from './components/Contact'
+import Procedures from './components/Procedures';
+import About from './components/About'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './styles.css';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<App /> }>
+      <Route path='/' element ={<Home />} />
+      <Route path='Procedures' element ={<Procedures />} />
+      <Route path='About' element ={<About />} />
+      <Route path='Contact' element={<Contact />} />
+    </Route>
+
+    </Routes>
+  
+  </BrowserRouter>,
+  document.getElementById('root')
+)
